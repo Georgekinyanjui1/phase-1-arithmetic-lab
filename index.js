7c4b34a14bb772083f04cd7e7bda4da99d021340
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-07 19:14:57 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 15.5
+const num2 = 4
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10)
+const num3 = 24
+const num4 = 5
+const mod = num3 % num4
+const max = Math.max(20)
